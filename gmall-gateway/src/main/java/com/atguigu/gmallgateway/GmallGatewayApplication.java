cf06e1fa11e3d873e8fc8fc2ce26c8925a1db9bc
zhanglu <2655494421@qq.com>
2020-05-28 18:32:05 +0800

    '项目初步搭建'

---
+++ gmall-gateway/src/main/java/com/atguigu/gmallgateway/GmallGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmallgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+public class GmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallGatewayApplication.class, args);
+    }
+
+}
